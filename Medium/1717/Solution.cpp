89fa3a8044a5597d10a5bec8a92dd5e38bdcc52e
LJFP <ljfp@ljfp.xyz>
2024-07-16 11:49:50 +0200

    Refactor 1717 to make it easier to read. The lambda function now is a private class method.

--- Medium/1717/Solution.cpp
+++ Medium/1717/Solution.cpp
@@ -4,52 +4,37 @@
 
 class Solution {
 public:
-    int maximumGain(string s, int x, int y) {
-        // Initialize variables and determine which patterns ('ab' or 'ba') are going to be the high and low rewards.
-        uint32_t finalScore = 0;
-        tuple<char, char, uint16_t> highReward, lowReward;
-        if ( x > y )
-        {
-            highReward = make_tuple('a', 'b', x);
-            lowReward = make_tuple('b', 'a', y);
-        }
-        else
-        {
-            highReward = make_tuple('b', 'a', y);
-            lowReward = make_tuple('a', 'b', x);
-        }
+	int maximumGain(string s, int x, int y) {
+		// Initialize variables and determine which patterns ('ab' or 'ba') are going to be the high and low rewards.
+		tuple<char, char, uint16_t> highReward = x > y ? make_tuple('a', 'b', x) : make_tuple('b', 'a', y);
+		tuple<char, char, uint16_t> lowReward = x > y ? make_tuple('b', 'a', y) : make_tuple('a', 'b', x);
 
-        auto scoreString = [](string &s, char first, char second, uint16_t reward, bool shouldResize) -> uint32_t {
-            uint32_t score = 0;
-            uint32_t i = 0;  // Index to keep track of the end of the valid substring
+		uint32_t finalScore = 0;
+		finalScore += scoreString(s, get<0>(highReward), get<1>(highReward), get<2>(highReward), true); // Score high reward first
+		finalScore += scoreString(s, get<0>(lowReward), get<1>(lowReward), get<2>(lowReward), false); // Score low reward with the filtered string.
 
-            for (char c : s)
-            {
-                if (i > 0 && s[i - 1] == first && c == second)
-                {
-                    score += reward;
-                    i--;  // Remove the matched pattern
-                } 
-                else
-                {
-                    s[i++] = c;  // Move valid characters to the front
-                }
-            }
+		return finalScore;
+	}
+private:
+	uint32_t scoreString(string& s, char first, char second, uint16_t reward, bool shouldResize) {
+		uint32_t score = 0;
+		uint32_t writeIndex = 0; // Index to keep track of the end of the valid substring
 
-            // Resize the string to the valid part if you're going to score again.
-            if (shouldResize)
-            {
-                s.resize(i);
-            }
-            return score;
-        };
+		for (char c : s ) {
+			if ( writeIndex > 0 && s[writeIndex - 1] == first && c == second ) {
+				score += reward;
+				writeIndex--; // Remove the matched pattern
+			}
+			else {
+				s[writeIndex++] = c; // Move valid characters to the front
+			}
+		}
 
-        // Score high reward first
-        finalScore += scoreString(s, get<0>(highReward), get<1>(highReward), get<2>(highReward), true);
+		// Resize the string to the valid part if you're going to score again.
+		if (shouldResize) {
+			s.resize(writeIndex);
+		}
 
-        // Score low reward with the filtered string.
-        finalScore += scoreString(s, get<0>(lowReward), get<1>(lowReward), get<2>(lowReward), false);
-
-        return finalScore;
-    }
+		return score;
+	}
 };
